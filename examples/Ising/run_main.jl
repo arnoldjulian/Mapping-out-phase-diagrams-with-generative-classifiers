269d771d00eca06a0df0bea1a172e67a9c7ef185
Julian Arnold <julian.arnoldd@gmail.com>
2023-06-07 23:54:57 +0200

    v1

---
+++ examples/Ising/run_main.jl
@@ -0,0 +1,168 @@
+# activate project
+cd(@__DIR__)
+using Pkg; Pkg.activate("../..")
+
+# load packages
+using GenClassifierPT
+using DelimitedFiles
+using LaTeXStrings
+using Plots
+using Random
+ENV["GKSwstype"]="nul"
+using JLD
+
+# choose system size
+L = 20
+
+# set path to data folder
+data_save_folder = "../../data/Ising/L="*string(L)*"/"
+
+# define parameter ranges
+γ1_min = -1.475f0
+γ1_max = 1.475f0
+γ2_min = -1.475f0
+γ2_max = 1.475f0
+dγ1 = 0.05f0
+dγ2 = 0.05f0
+γ1_range = collect(γ1_min:dγ1:γ1_max)
+γ2_range = collect(γ2_min:dγ2:γ2_max)
+points = vcat(collect(Iterators.product(γ1_range, γ2_range))...)
+γ1_range_LBC = collect(γ1_min-dγ1/2:dγ1:γ1_max+dγ1/2)
+γ2_range_LBC = collect(γ2_min-dγ2/2:dγ2:γ2_max+dγ2/2)
+
+# load data
+
+# x_data is of size length(γ1_range) x length(γ2_range) x size of state space
+# and contains the distribution over the sufficient statistic (here, they take on 39'571 distinct/unique values) at each sampled point in parameter space,
+# i.e., the relevant set of generative models
+x_data = load(data_save_folder*"x_data.jld")["x_data"]
+
+# set of distinct samples
+samples = load(data_save_folder*"samples.jld")["samples"]
+n_samples = length(samples)
+
+# p_data of size length(γ1_range) x length(γ2_range) x number of tuning parameters and stores the value of all sampled points in parameter space
+p_data = load(data_save_folder*"p_data.jld")["p_data"]
+
+# load analytical reference phase boundary
+ref_phase_boundary = load(data_save_folder*"ref_phase_boundary.jld")["ref_phase_boundary"]
+
+##########
+# scheme 1
+##########
+
+# define classes for scheme 1
+n_classes = 5
+class_data = zeros(eltype(n_classes),length(γ1_range),length(γ2_range),n_classes)
+for class_indx in 1:n_classes
+	if class_indx == 1
+        class_data[10,10,class_indx] = 1
+	elseif class_indx == 2
+        class_data[10,length(γ2_range)-10,class_indx] = 1
+	elseif class_indx == 3
+        class_data[length(γ1_range)-10,10,class_indx] = 1
+	elseif class_indx == 4
+        class_data[length(γ1_range)-10,length(γ2_range)-10,class_indx] = 1
+	else
+		class_data[Int(length(γ1_range)/2),Int(length(γ2_range)/2),class_indx] = 1
+	end
+end
+
+# obtain indicator of scheme 1 based on set of generative models
+I_1 = GCPT.run_scheme_1(x_data, class_data, [dγ1, dγ2])
+
+# plotting routine
+function f(γ1,γ2,I_1)
+	γ1_index = findall(x->x==γ1, γ1_range[2:end-1])[1]
+	γ2_index = findall(x->x==γ2, γ2_range[2:end-1])[1]
+    return I_1[γ1_index, γ2_index]
+end
+
+Plots.pyplot()
+x = γ1_range[2:end-1]
+y = γ2_range[2:end-1]
+
+X = repeat(reshape(x, 1, :), length(y), 1)
+Y = repeat(y, 1, length(x))
+Z = map((x,y)->f(x,y,I_1), X, Y)
+
+plt = contour(x, y, (x,y)->f(x,y,I_1), fill=true, dpi=300, color=:thermal)
+xlabel!(L"J_{x}/k_{\mathrm{B}}T}")
+ylabel!(L"J_{y}/k_{\mathrm{B}}T}")
+plot!(ref_phase_boundary[:,1,1],ref_phase_boundary[:,2,1],color="green",label="analytical")
+plot!(ref_phase_boundary[:,1,2],ref_phase_boundary[:,2,2],color="green",label=false)
+plot!(ref_phase_boundary[:,1,3],ref_phase_boundary[:,2,3],color="green",label=false)
+plot!(ref_phase_boundary[:,1,4],ref_phase_boundary[:,2,4],color="green",label=false)
+xlims!((γ1_range[2],γ1_range[end-1]))
+ylims!((γ2_range[2],γ2_range[end-1]))
+#savefig("./results_ising_scheme_1.png")
+
+##########
+# scheme 2
+##########
+
+# define l parameter to choose for scheme 2
+l_param = 2
+
+# compute indicator of scheme 2 based on set of generative models
+I_2 = GCPT.run_scheme_2(x_data,l_param,γ1_range,γ1_range_LBC,γ2_range,γ2_range_LBC,[dγ1,dγ2])
+
+# plotting routine
+function f(γ1,γ2,I_2)
+	γ1_index = findall(x->x==γ1, γ1_range[2:end-1])[1]
+	γ2_index = findall(x->x==γ2, γ2_range_LBC[2:end-1])[1]
+	return I_2[γ1_index, γ2_index]
+end
+
+Plots.pyplot()
+x = γ1_range[2:end-1]
+y = γ2_range_LBC[2:end-1]
+
+X = repeat(reshape(x, 1, :), length(y), 1)
+Y = repeat(y, 1, length(x))
+Z = map((x,y)-> f(x,y,I_2), X, Y)
+
+plt = contourf(x, y, (x,y)->f(x,y,I_2), fill=true,dpi=300,color=:thermal,levels=1000)
+xlabel!(L"J_{x}/k_{\mathrm{B}}T}")
+ylabel!(L"J_{y}/k_{\mathrm{B}}T}")
+plot!(ref_phase_boundary[:,1,1],ref_phase_boundary[:,2,1],color="green",label="analytical")
+plot!(ref_phase_boundary[:,1,2],ref_phase_boundary[:,2,2],color="green",label=false)
+plot!(ref_phase_boundary[:,1,3],ref_phase_boundary[:,2,3],color="green",label=false)
+plot!(ref_phase_boundary[:,1,4],ref_phase_boundary[:,2,4],color="green",label=false)
+xlims!((γ1_range[2],γ1_range[end-1]))
+ylims!((γ2_range[2],γ2_range[end-1]))
+#savefig("./results_ising_scheme_2.png")
+
+##########
+# scheme 3
+##########
+
+# compute indicator of scheme 3 based on set of generative models
+#_, _, I_3 = GCPT.run_scheme_3(x_data, p_data, [dγ1, dγ2])
+I_3 = GCPT.run_scheme_3_linescans(x_data, p_data, [dγ1, dγ2])
+
+# plotting routine
+function f(γ1,γ2,I_3)
+	γ1_index = findall(x->x==γ1, γ1_range[2:end-1])[1]
+	γ2_index = findall(x->x==γ2, γ2_range[2:end-1])[1]
+	return I_3[γ1_index, γ2_index]
+end
+
+Plots.pyplot()
+x = γ1_range[2:end-1]
+y = γ2_range[2:end-1]
+
+X = repeat(reshape(x, 1, :), length(y), 1)
+Y = repeat(y, 1, length(x))
+Z = map((x,y)->f(x,y,I_3), X, Y)
+
+plt = contourf(x, y, (x,y)->f(x,y,I_3), fill=true,dpi=300,color=:thermal,levels=1000)
+xlabel!(L"J_{x}/k_{\mathrm{B}}T}")
+ylabel!(L"J_{y}/k_{\mathrm{B}}T}")
+plot!(ref_phase_boundary[:,1,1],ref_phase_boundary[:,2,1],color="green",label="analytical")
+plot!(ref_phase_boundary[:,1,2],ref_phase_boundary[:,2,2],color="green",label=false)
+plot!(ref_phase_boundary[:,1,3],ref_phase_boundary[:,2,3],color="green",label=false)
+plot!(ref_phase_boundary[:,1,4],ref_phase_boundary[:,2,4],color="green",label=false)
+xlims!((γ1_range[2],γ1_range[end-1]))
+ylims!((γ2_range[2],γ2_range[end-1]))
+#savefig("./results_ising_scheme_3.png")
---
+++ examples/Ising/run_snake.jl
@@ -0,0 +1,114 @@
+# activate project
+cd(@__DIR__)
+using Pkg; Pkg.activate("../..")
+
+# load packages
+using GenClassifierPT
+using DelimitedFiles
+using LaTeXStrings
+using Plots
+using Random
+ENV["GKSwstype"]="nul"
+using JLD
+using Flux
+
+# choose system size
+L = 20
+
+# set path to data folder
+data_save_folder = "../../data/Ising/L="*string(L)*"/"
+
+# define parameter ranges
+p1_min = -1.475f0
+p1_max = 1.475f0
+p2_min = -1.475f0
+p2_max = 1.475f0
+dp1 = 0.05f0
+dp2 = 0.05f0
+p1_range = collect(p1_min:dp1:p1_max)
+p2_range = collect(p2_min:dp2:p2_max)
+
+rescale_x = p -> GCPT.rescale(p,p1_range)
+rescale_y = p -> GCPT.rescale(p,p2_range)
+
+inv_rescale_x = p -> GCPT.inv_rescale(p,p1_range)
+inv_rescale_y = p -> GCPT.inv_rescale(p,p2_range)
+
+p1_range_ren = rescale_x.(p1_range)
+p2_range_ren = rescale_y.(p2_range)
+dp1_ren = p1_range_ren[2]-p1_range_ren[1]
+dp2_ren = p2_range_ren[2]-p2_range_ren[1]
+
+# load data
+
+# x_data is of size length(p1_range) x length(p2_range) x size of state space
+# and contains the distribution over the sufficient statistic (here, they take on 39'571 distinct/unique values) at each sampled point in parameter space,
+# i.e., the relevant set of generative models
+x_data = load(data_save_folder*"x_data.jld")["x_data"]
+
+# set of distinct samples
+samples = load(data_save_folder*"samples.jld")["samples"]
+n_samples = length(samples)
+
+# p_data of size length(p1_range) x length(p2_range) x number of tuning parameters and stores the value of all sampled points in parameter space
+p_data = load(data_save_folder*"p_data.jld")["p_data"]
+
+# load analytical reference phase boundary
+ref_phase_boundary = load(data_save_folder*"ref_phase_boundary.jld")["ref_phase_boundary"]
+
+###########################################
+# snake scheme using generative classifiers
+###########################################
+
+# set boundaries of the snake
+boundaries = zeros(eltype(p1_min),(2,2))
+boundaries[1,:] = [dp1_ren,1.0f0-dp1_ren]
+boundaries[2,:] = [dp2_ren,1.0f0-dp2_ren]
+
+# define vertices/nodes of the snake 
+function def_vertices(p1_range_ren,p2_range_ren)
+    n_vertices = 20
+    vertices = zeros(Float32,n_vertices,2)
+    for i in 1:n_vertices
+        vertices[i,:]  = [p1_range_ren[36+i],p2_range_ren[59-i]]
+    end
+    return vertices
+end
+
+# set hyperparameters
+lr_int = 0.0001f0
+lr_ext = 0.0005f0
+n_nodes=4
+w0=5*maximum([dp1_ren,dp2_ren])
+w_end=minimum([dp1_ren,dp2_ren])
+w_decay = 0.90f0
+opt_internal = Adam(lr_int)
+opt_external = Adam(lr_ext)
+α=0.002f0
+β=0.4f0
+n_epochs = 400
+
+# initialize snake
+snake = GCPT.initialize_snake(def_vertices(p1_range_ren,p2_range_ren),PBC=false,FBC=false,boundaries,p1_range_ren,p2_range_ren,n_nodes=n_nodes,α=α,β=β,w0=w0,w_end=w_end,w_decay=w_decay,reshape=false,train_local=false,max_move=false,max_moves=[5*dp1,5*dp2],treat_boundary=false)
+
+# plot reference phase boundary and initial snake
+plot(ref_phase_boundary[:,1,1],ref_phase_boundary[:,2,1],color="green",label="analytical",legend=:topleft)
+plot!(ref_phase_boundary[:,1,2],ref_phase_boundary[:,2,2],color="green",label=false)
+plot!(ref_phase_boundary[:,1,3],ref_phase_boundary[:,2,3],color="green",label=false)
+plot!(ref_phase_boundary[:,1,4],ref_phase_boundary[:,2,4],color="green",label=false)
+scatter!(inv_rescale_x.(snake.vertices[:,1]),inv_rescale_y.(snake.vertices[:,2]),label="Snake: Epoch 0",color="black")
+
+# train the snake (and plot its position)
+colors = palette([:yellow, :red], n_epochs)
+for epoch in 1:n_epochs
+    GCPT.update_snake!(snake, x_data, n_samples, opt_internal, opt_external,epoch)
+    if epoch % 50 == 0
+        println(epoch)
+        scatter!(inv_rescale_x.(snake.vertices[:,1]),inv_rescale_y.(snake.vertices[:,2]),label="Snake: Epoch "*string(epoch),color=colors[epoch])
+        plot!(inv_rescale_x.(snake.vertices[:,1]),inv_rescale_y.(snake.vertices[:,2]),label=false,color=colors[epoch])
+    end
+end
+xlabel!(L"J_{x}/k_{\mathrm{B}}T}")
+ylabel!(L"J_{y}/k_{\mathrm{B}}T}")
+current()
+#savefig("./results_ising_snake.png")
